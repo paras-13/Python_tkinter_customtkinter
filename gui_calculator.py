77f4cc32e9b284ad211085d8e990b1c2e98e7b20
Paras Upadhyay <paras13upadhyay@gmail.com>
2023-07-30 17:37:57 +0530

    GUI Calculator

---
+++ gui_calculator.py
@@ -0,0 +1,8 @@
+# Python GUI calculator
+from tkinter import *
+from tkinter import messagebox as tmsg
+cal = Tk()
+cal.title("Calculator")
+cal.geometry("320x500")
+cal.wm_iconbitmap("Calicon.ico")
+cal.mainloop()
