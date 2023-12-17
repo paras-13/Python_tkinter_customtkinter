add4ac906d30761498bc2271650b0dec42f29830
Paras Upadhyay <paras13upadhyay@gmail.com>
2023-12-17 20:31:08 +0530

    BMI Calculator Python GUI with customTkinter

---
+++ BMI_Calculator/bmi_calculator.py
@@ -0,0 +1,8 @@
+import customtkinter as ctk
+win=ctk.CTk();
+win.title("BMI Calculator")
+win.iconbitmap("D:\\Python\\Python Git\\Python_tkinter_customtkinter\\BMI_Calculator\\bmi.ico")
+win.geometry("500x300")
+win.maxsize(500,300)
+win.minsize(500,300)
+win.mainloop();
