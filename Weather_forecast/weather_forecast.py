c0c6fbc17903f25a891c6fbb7fd74b5d15500adc
Paras Upadhyay <paras13upadhyay@gmail.com>
2023-12-31 23:33:41 +0530

    Weather Forecast gui

--- Weather_forecast/weather_forecast.py
+++ Weather_forecast/weather_forecast.py
@@ -1,1 +1,10 @@
-import customtkinter as stk
+import customtkinter as ctk
+app = ctk.CTk()
+app.geometry("600x500")
+app.maxsize(600, 500)
+app.minsize(600, 500)
+app.title("Weather Forecast")
+app.iconbitmap("")
+
+
+app.mainloop()
